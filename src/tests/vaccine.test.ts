05a00b1e174d435ae9056b8009d715bdda5947c8
Kostix <averskos@gmail.com>
2023-02-17 16:00:23 +0200

    fix: extra dep

--- src/tests/vaccine.test.ts
+++ src/tests/vaccine.test.ts
@@ -1,8 +1,5 @@
 import request from 'supertest';
 import App from '@/app';
-import { PetsController } from '@controllers/vaccine.controller';
-import { Pet } from '@interfaces/vaccine.interface';
-import petModel from '@models/vaccine.model';
 
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
